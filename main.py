ab31dee7e89b186101a091274b4fec8d7f59ca22
Marcin <persky03@gmail.com>
2021-03-18 08:45:49 +0100

    Homepage changes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# JS
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 if __name__ == '__main__':
     app.run()
